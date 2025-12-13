c472d79ae338b78d7d96cdbd4c368816208b376b
grrogn <grrogn01@gmail.com>
2025-12-13 14:24:13 +0300

    mobile profile

--- mobile.js
+++ mobile.js
@@ -6,13 +6,15 @@ const reg_button = document.getElementById("reg_button_mobile");
 const navbar_home_button = document.getElementById("navbar_home_button");
 const navbar_categories_button = document.getElementById("navbar_categories_button");
 const navbar_player_button = document.getElementById("navbar_player_button");
+const navbar_profile_button = document.getElementById("navbar_my_music_button");
 const categories__content = document.getElementById("categories__content");
 const home__content = document.getElementById("home__content");
 const player__content = document.getElementById("player__content");
-
+const profile__content = document.getElementById("profile__content");
 
 categories__content.classList.add('hidden');
 player__content.classList.add('hidden');
+profile__content.classList.add('hidden');
 
 burger_button.addEventListener("click", () => {
         curtain_header.style.top = "0";
@@ -54,8 +56,10 @@ navbar_categories_button.addEventListener("click", () => {
     categories__content.classList.remove('hidden');
     home__content.classList.add('hidden');
     player__content.classList.add('hidden');
+    profile__content.classList.add('hidden');
     navbar_home_button.classList.remove('active');
     navbar_player_button.classList.remove('active');
+    navbar_profile_button.classList.remove('active');
     navbar_categories_button.classList.add('active');
     currentWindow = 'categories__content';
 });
@@ -64,22 +68,37 @@ navbar_home_button.addEventListener("click", () => {
     
     player__content.classList.add('hidden');
     categories__content.classList.add('hidden');
+    profile__content.classList.add('hidden');
     home__content.classList.remove('hidden');
     navbar_player_button.classList.remove('active');
     navbar_categories_button.classList.remove('active');
+    navbar_profile_button.classList.remove('active');
     navbar_home_button.classList.add('active');
     currentWindow = 'home__content';
 });
 
 navbar_player_button.addEventListener("click", () => {
-   
     categories__content.classList.add('hidden');
     home__content.classList.add('hidden');
+    profile__content.classList.add('hidden');
     player__content.classList.remove('hidden');
     navbar_player_button.classList.add('active');
+    navbar_profile_button.classList.remove('active');
     navbar_home_button.classList.remove('active');
     navbar_categories_button.classList.remove('active');
     currentWindow = 'player__content';
+});
+
+navbar_profile_button.addEventListener("click", () => {
+    categories__content.classList.add('hidden');
+    home__content.classList.add('hidden');
+    player__content.classList.add('hidden');
+    profile__content.classList.remove('hidden');
+    navbar_profile_button.classList.add('active');
+    navbar_home_button.classList.remove('active');
+    navbar_categories_button.classList.remove('active');
+    navbar_player_button.classList.remove('active');
+    currentWindow = 'profile__content';
 });
 
 
